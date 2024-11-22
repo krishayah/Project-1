e6bb272b89b3d5a11cce03a8a3628ba1046f31b8
krishayah <krishagurung26@gmail.com>
2024-11-21 18:24:59 -0600

    Update test_television.py

--- test_television.py
+++ test_television.py
@@ -3,5 +3,20 @@ from television import *
 
 def test_innit():
     TV = Television()
-    #test if TV initializes w/ correct default values
-    assert str(tv) == "Power = False, Channel = 0, Volume = 0"
+    #test if TV initializes w/ correctly (TV off, channel 0, volume 0)
+    assert str(TV) == "Power = False, Channel = 0, Volume = 0"
+
+def test_power_ON():
+    TV = Television()
+
+    TV.power() #test turning it on
+    assert str(TV) == "Power = True, Channel = 0, Volume = 0"
+
+    TV.power() #test turning it OFF
+    assert str(TV) == "Power = False, Channel = 0, Volume = 0"
+
+def test_mute():
+    TV = Television()
+
+    
+
