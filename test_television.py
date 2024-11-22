40c094b03b85a8b6b90efda0a47c42a19a62d0db
krishayah <krishagurung26@gmail.com>
2024-11-21 18:50:42 -0600

    added more tests

--- test_television.py
+++ test_television.py
@@ -32,5 +32,45 @@ def test_mute():
     TV.mute()
     assert str(TV) == "Power = False, Channel = 0, Volume = 0"  #WHen TV's off, VOlume = 0
 
+#test Channel_up method
+def test_channel_up():
+    TV = Television()
+
+    #test increasing channel when TV's OFF
+    TV. channel_up()
+    assert str(TV) == "Power = False, Channel = 0, Volume = 0"
+
+    #test increasing channel when TV's ON
+    TV.power()  #to turn ON
+    TV.channel_up()  #i ^ channel
+    assert str(TV) == "Power = True, Channel = 1, Volume = 0"
+
+    #test increasing channel past Max Value
+    for _ in range (4):
+        TV.channel_up()
+    assert str(TV) == "Power = True, Channel = 0, Volume = 0"
+
+#test channel_down method
+def test_channel_down():
+    TV = Television()
+
+    #Test decreasing channel when TV's OFF
+    TV. channel_down()
+    assert str(TV) == "Power = False, Channel = 0, Volume = 0"
+
+    #test decreasing channel when TV's off
+    TV.power() 
+    TV.channel_down() 
+    assert str(TV) == "Power = True, Channel = 3, Volume = 0"  # Wraps around to MAX_CHANNEL (3)
+
+    #test decreasing channel past the Minimum value (wraps around)
+    TV.channel_down()
+    assert str(TV) == "Power = True, Channel = 3, Volume = 0"
+   
+
+
+
+
+
 
 
